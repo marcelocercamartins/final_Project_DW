a4646f44db16b18e96107e8228ed3110a48faa80
Miguel <miguelrr314@gmail.com>
2023-10-27 20:03:05 +0100

    Our first API!

---
+++ API/apiApp/Controllers/PersonsEverywhere.cs
@@ -0,0 +1,31 @@
+﻿using apiApp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiApp.Controllers
+{
+    [Route("api/Persons")]
+    [ApiController]
+    public class PersonsEverywhere : ControllerBase
+    {
+        public PersonsEverywhere()
+        {
+            
+        }
+
+
+        [HttpGet]
+        public person getTest()
+        {
+            return new person { Id = 1, Name = "Michael" };
+        }
+
+        [HttpPut]
+        public void createPerson()
+        {
+            List<person> persons = new List<person>();
+           foreach(var item in persons) { }
+        }
+
+    }
+}
---
+++ API/apiApp/Model/person.cs
@@ -0,0 +1,8 @@
+﻿namespace apiApp.Model
+{
+    public class person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
