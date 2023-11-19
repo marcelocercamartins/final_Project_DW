054fd1747cbbd527ad1e628e2c2a1facb1853b8b
Miguel <miguelrr314@gmail.com>
2023-11-19 15:42:36 +0000

    database update model

---
+++ prisma/migrations/20231119153830_migrate19111538/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `eventAdress` on the `Events` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Events" DROP COLUMN "eventAdress";
