9f2516804c3f1074a2beaa6cd856207a71f4fc55
Miguel <miguelrr314@gmail.com>
2023-12-08 19:30:13 +0000

    jwt working + mongo db init (by xurumbambo)

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+const {MongoClient, ServerApiVersion} = require('mongodb');
 
 const app = express();
-
-const PORT = 3000;
-const SECRET = "fdçlkjdflkjdslfkgjdfkgºpçdfkpdrkgpdrgjkpfkmfdçl";
-
+const PORT = process.env.PORT;
+const secret = process.env.SECRET;
+const uri = process.env.DBURI;
 
 app.use(cors());
-app.options('*', cors()) // include before other routes
-
+app.options('*', cors())
 app.use(express());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,62 +21,53 @@ let users = require('./db/users');
 let dados = require('./db/dados');
 let favoritos = require('./db/favoritos');
 
-function escreve(fich, db) {
-    fs.writeFile(fich, JSON.stringify(db, null, 4), 'utf8', err => {
-        if (err) {
-            console.log(`Error writing file: ${err}`)
-        } else {
-            console.log('Escreveu no ficheiro ' + fich); // Sucesso
-        }
-    })
-}
+const dbConn = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
-function existeUser(nome) {
-    for (utilizador of users)
-        if (utilizador.username === nome) {
-            return true;
-        }
-    return false;
-}
-//
-// Registo de um novo utilizador
-app.post("/registar", (req, res) => {
+app.get("/getToken", (req, res) => {
+    res.send()
+
+});
+
+// Create new User
+app.post("/register", (req, res) => {
     const username = req.body.username;
-    if (!existeUser(username)) {
-        const newUser = {
-            username: username,
-            password: req.body.password,
-            tipo: 0
+    const password = req.body.password;
+
+    if (!verifyIfUserExists(username)) 
+    {
+        if (password.length < 5) 
+        {
+            return res.status(400).send({msg: 'Password deve ter 5 ou mais caracteres'});
         }
-        if (newUser.password.length < 5) {
-            return res.status(400).send({
-                msg: 'Password deve ter 5 ou mais caracteres'
-            });
-        }
+
+        const newUser = {sername: username, password: req.body.password, tipo: 0}
         users.push(newUser);
-        escreve("./db/users.json", users);
-        return res.status(201).send({
-            msg: `Criado utilizador ${username}`
-        });
-    } else {
-        return res.status(409).send({
-            msg: 'Utilizador já existe'
-        });
+        write("./db/users.json", users);
+
+        return res.status(201).send({msg: `Criado utilizador ${username}`});
+    } else 
+    {
+        return res.status(409).send({msg: 'Utilizador já existe'});
     }
 });
 
+
 // Login
 app.post("/login", (req, res) => {
-    const nome = req.body.username;
-    const senha = req.body.password;
-    for (utilizador of users) {
-        if (utilizador.username === nome)
-            if (utilizador.password === senha) {
-                token = jwt.sign(utilizador, SECRET);
-                return res.status(201).json({ 
-                    auth: true, 
-                    token: token,
-                msg: getFavoritos(utilizador.username) })
+    const name = req.body.username;
+    const password = req.body.password;
+ 
+    for (user of users) {
+        if (user.username === name)
+            if (user.password === password) {
+                token = jwt.sign(user, secret);
+                return res.status(201).json({ auth: true, token: token, msg: getFavoritos(user.username) })
             } else {
                 return res.status(401).json({ msg: "Password inválida!" })
             }
@@ -84,17 +75,9 @@ app.post("/login", (req, res) => {
     return res.status(404).json({ msg: "Utilizador não encontrado!" })
 });
 
-function validarToken(token) {
-    try {
-        return jwt.verify(token, SECRET);
-    } catch (err) {
-        return false;
-    }
-}
-
 // Acesso à informação somente se autorizado
 app.get("/listarDados", (req, res) => {
-    const decoded = validarToken(req.header('token'));
+    const decoded = verifyToken(req.header('token'));
     if (!decoded) {
         return res.status(401).json({ msg: "Utilizador não autenticado ou não autorizado!" });
     }
@@ -105,6 +88,14 @@ app.get("/listarDados", (req, res) => {
         res.status(404).json({ msg: "Dados não encontrados!" });
     }
 });
+
+function verifyToken(token) {
+    try {
+        return jwt.verify(token, secret);
+    } catch (err) {
+        return false;
+    }
+}
 
 function getFavoritos(username) {
     let lista = "<h3>Favoritos do utilizador " + username + "</h3><br>";
@@ -121,6 +112,42 @@ function getFavoritos(username) {
     }
     return lista;
 }
+
+function write(fich, db) {
+    fs.writeFile(fich, JSON.stringify(db, null, 4), 'utf8', err => {
+        if (err) {
+            console.log(`Error writing file: ${err}`)
+        } else {
+            console.log('Escreveu no ficheiro ' + fich); // Sucesso
+        }
+    })
+}
+
+function verifyIfUserExists(nome) {
+    for (user of users)
+        if (user.username === nome) {
+            return true;
+        }
+    return false;
+}
+
+
+async function connectToDB() {
+  try {
+    await dbConn.connect();
+    await dbConn.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }catch(err){
+        console.log(err);
+    }
+}
+
+async function disconnectToBD()
+{
+    await dbConn.close();
+}
+
+
 
 app.use(express.static('public'));
 app.listen(PORT, () => {
