b391c090d2122d29144fcab28e98d07de74036ad
Bernardo Pires <bernardopires302@gmail.com>
2023-12-08 11:41:49 +0000

    layout update
    adicionei função para verificar o login (incompleto) para definir os botões da navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,18 +8,9 @@ function MainLayout({ children }: Props) {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
 
-  const navbarButtons = [{
-    id: "1", name: "Home", url: "/" /* fica apenas / para ele voltar ao index, que está a ser usado como home page */
-  }, {
-    id: "2", name: "Eventos", url: "events"
-  }, {
-    id: "3", name: "Meus Eventos", url: "myEvents"
-  }]
+  const navbarButtons = defineNavbarButtons()
 
-  const UserLogged = verifyLogin()
-
-  if (UserLogged == 0){
-    return (
+  return (
       <AppShell
         header={{ height: 60 }}
         navbar={{
@@ -39,6 +30,7 @@ function MainLayout({ children }: Props) {
         <AppShell.Navbar p="md">
           <Flex
             mih={255}
+            h="100%"
             gap="md"
             justify="flex-start"
             align="center"
@@ -47,43 +39,6 @@ function MainLayout({ children }: Props) {
             {navbarButtons.map((item) => (
               <Button id={item.id} variant="filled" radius="xl" mt="md" w={200} p={5} component='a' href={item.url}>{item.name}</Button>
             ))}
-            <Button variant="filled" color="green" radius="xl" mt="md" w={200} p={5} component='a' href='login'>Login</Button>
-            <Button variant="transparent" color="indigo" radius="xl" mt="md" w={200} p={5} component='a' href='createAccount'>Criar Conta</Button>
-          </Flex>
-        </AppShell.Navbar>
-        <AppShell.Main>{children}</AppShell.Main>
-      </AppShell>
-    );
-  } else {
-    return (
-      <AppShell
-        header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: 'sm',
-          collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-        }}
-        padding="md"
-      >
-        <AppShell.Header>
-          <Group h="100%" px="md">
-            <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-            <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-            <Image component={NextImage} radius="md" h={55} w={55} fit="contain" src={Logo} alt="My image" /> Event Eagle
-          </Group>
-        </AppShell.Header>
-        <AppShell.Navbar p="md">
-          <Flex
-            mih={255}
-            gap="md"
-            justify="flex-start"
-            align="center"
-            direction="column"
-          >
-            {navbarButtons.map((item) => (
-              <Button id={item.id} variant="filled" radius="xl" mt="md" w={200} p={5} component='a' href={item.url}>{item.name}</Button>
-            ))}
-            <Button variant="filled" color="green" radius="xl" mt="md" w={200} p={5} component='a' href='login'>Log Out</Button>
           </Flex>
         </AppShell.Navbar>
         <AppShell.Main>{children}</AppShell.Main>
@@ -91,10 +46,36 @@ function MainLayout({ children }: Props) {
     );
   }
 
-}
-
 export default MainLayout;
 
-function verifyLogin(){
-    return 1;
+function defineNavbarButtons(){
+  let UserLogged = 0;
+  // codigo para fazer verificação de login
+  
+  if (UserLogged == 0){
+    const navbarButtons = [{
+      id: "1", name: "Home", url: "/" /* fica apenas / para ele voltar ao index, que está a ser usado como home page */
+    }, {
+      id: "2", name: "Eventos", url: "events"
+    }, {
+      id: "3", name: "Login", url: "login"
+    }, {
+      id: "4", name: "Criar Conta", url: "createAccount"
+    }]
+
+    return navbarButtons
+
+  }else{
+    const navbarButtons = [{
+      id: "1", name: "Home", url: "/" /* fica apenas / para ele voltar ao index, que está a ser usado como home page */
+    }, {
+      id: "2", name: "Eventos", url: "events"
+    }, {
+      id: "3", name: "Meus Eventos", url: "myEvents"
+    }, {
+      id: "4", name: "Logout", url: "login"
+    }]
+
+    return navbarButtons
+  }
 }
