1ac021e3e5302e814d89660b1d7868bdd68f8cf8
Bernardo Pires <bernardopires302@gmail.com>
2024-01-19 23:03:59 +0000

    melhoria na responsividade da lista de eventos
    falta para ecras moveis

--- Client/scripts/eventsList.js
+++ Client/scripts/eventsList.js
@@ -52,7 +52,7 @@ function createEventsList(eventsList){
                 //configuração da div principal
                 mainDiv.className = "mainDiv";
                 mainDiv.style.width = "80%";
-                mainDiv.style.height = "100px";
+                mainDiv.style.height = "120px";
                 mainDiv.style.marginTop = "20px";
                 mainDiv.style.marginBottom = "20px";
                 mainDiv.style.marginLeft = "10%";
@@ -61,22 +61,23 @@ function createEventsList(eventsList){
                 mainDiv.style.padding = "10px";
                 mainDiv.style.border = "1px solid pink";
                 mainDiv.style.borderRadius = "10px";
-
+                
                 //left div contem a imagem do evento
                 const leftDiv = document.createElement("div");
 
                 //configuração da div 
                 leftDiv.className = "leftDiv";
-                leftDiv.style.width = "10%";
+                leftDiv.style.flex = "0 0 10%";
                 leftDiv.style.height = "100%";
                 leftDiv.style.paddingLeft = "20px";
+                leftDiv.style.paddingTop = "5px";
 
                 //configuração da imagem
                 const eventImage = document.createElement("img");
                 //eventImage.alt = value.name;     texto que aparece quando se dá hover à imagem
                 eventImage.src = value.imageURL;
-                eventImage.style.width = "80px";
-                eventImage.style.height = "80px";
+                eventImage.style.width = "90px";
+                eventImage.style.height = "90px";
                 leftDiv.appendChild(eventImage);
 
                 //adição à div principal
@@ -86,12 +87,25 @@ function createEventsList(eventsList){
                 const rightDiv = document.createElement("div");
                 //configuração da div
                 rightDiv.className = "rightDiv";
-                rightDiv.style.width = "82%";
+                rightDiv.style.flex = "1";
                 rightDiv.style.height = "100%";
                 rightDiv.style.paddingLeft = "30px";
                 rightDiv.style.display = "flex";
                 rightDiv.style.alignItems = "center";
-                rightDiv.innerHTML = value.name + "<br>" + eventDay + " | | " + eventHour + "<br>" + value.location
+                rightDiv.style.overflow = "hidden";  
+                rightDiv.innerHTML = value.name + "<br>" + eventDay + "<br>" + eventHour + "<br>" + value.location
+                
+                //ajusta os elementos em página web
+                window.addEventListener('resize', function () {
+                        if (window.innerWidth < 700) { 
+                            rightDiv.innerHTML = value.name + "<br>" + eventDay + "<br>" + eventHour;
+                        } else {
+                            rightDiv.innerHTML = value.name + "<br>" + eventDay + "<br>" + eventHour + "<br>" + value.location;
+                        }
+                });
+
+
+                
                 //adição à div principal
                 mainDiv.appendChild(rightDiv);
 
